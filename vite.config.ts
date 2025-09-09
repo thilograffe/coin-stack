f0b199a7f202d583d4ee1e46144875b6ee211b61
Thilo Graffe <thilograffe@gmail.com>
2025-09-09 11:23:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/coin-stack/",
+  build: {
+    outDir: "dist",
+    assetsDir: "./",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
 });
