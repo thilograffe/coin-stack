bc355f37d7abfc1926b84e0449b88244f50396f1
Thilo Graffe <thilograffe@gmail.com>
2025-09-09 10:35:11 +0200

    removed base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/coin-stack/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
